f93765dba34ff4e95ea06db54d1b1ab78b99b6f8
lporto21 <lporto@estudiante.ceibal.edu.uy>
2025-06-28 14:18:23 -0300

    Html y Css básico para el Inicio de sesión de usuario

---
+++ InicioSesión/InicioSesion.js
@@ -0,0 +1,8 @@
+function ingresoSocio() {
+    let userName = document.getElementById ("userName").value;
+    let userPassword = document.getElementById ("userPassword").value;
+
+    console.log("Nombre: "+userName+"\n");
+    console.log("CC: "+userPassword+"\n");
+
+}
