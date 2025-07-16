b5e6416532092ec1de224339749493a6b3bc8db6
lporto21 <lporto@estudiante.ceibal.edu.uy>
2025-07-15 22:29:18 -0300

    Home del Backoffice con tarjetas de navegacion

--- InicioSesión/InicioSesion.js
+++ InicioSesión/InicioSesion.js
@@ -1,8 +1,6 @@
 function ingresoSocio() {
     let cedula = document.getElementById ("cedula").value;
     let password = document.getElementById ("password").value;
-
-    console.log("cedula: "+cedula+"\n");
-    console.log("CC: "+password+"\n");
+    let error = document.getElementById ("error").value;
 
 }
