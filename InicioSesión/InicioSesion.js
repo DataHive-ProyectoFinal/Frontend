c6ec9ff986bd6a64b4f29deb9c178616b3fb0a85
lporto21 <lporto@estudiante.ceibal.edu.uy>
2025-07-22 22:46:17 -0300

    Creacion de el home de socios

--- InicioSesión/InicioSesion.js
+++ InicioSesión/InicioSesion.js
@@ -1,6 +1,8 @@
-function ingresoSocio() {
+function iniciarSesion() {
     let cedula = document.getElementById ("cedula").value;
     let password = document.getElementById ("password").value;
     let error = document.getElementById ("error").value;
+    
+    window.location.href = "../FrontCooperativa/homeSocio.html";
 
 }
