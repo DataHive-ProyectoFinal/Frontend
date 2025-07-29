432760c0c5b2ff6e23137764b2c1350d5d6bbc0d
Valentinaghn <166853861+Valentinaghn@users.noreply.github.com>
2025-07-28 22:54:42 -0300

    Frontend actualizado funcionando

---
+++ formulario-registro/Form.js
@@ -0,0 +1,23 @@
+ const form = document.getElementById('form-registro');
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const datos = Object.fromEntries(new FormData(form));
+
+      fetch('http://localhost:8080/registro.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+        .then(res => res.json())
+        .then(data => {
+          alert(data.mensaje || 'Registro completado');
+        })
+        .catch(err => {
+          alert('Error al registrar usuario');
+          console.error(err);
+        });
+    });
